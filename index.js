088636b0cac58f1421040809ec6098ad52f792f1
elsiri <ortizronald623@gmail.com>
2024-04-11 20:32:35 -0500

    Test cors

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ import dashboardRoutes from './routes/dashboard.routes.js'
 import loginRoutes from './routes/login.routes.js'
 const app = express()
 const _dirname = dirname(fileURLToPath(import.meta.url))
-app.use(cors());
+app.use(cors({
+    origin: 'https://constru-tech.up.railway.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
 app.use(express.json())
 app.use(express.urlencoded({
     extended:true
